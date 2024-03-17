4b70286b3d19a779516eb54c063827ee0ddd02b1
TerriblyFriedRice <148308017+TerriblyFriedRice@users.noreply.github.com>
2024-03-17 00:16:20 -0700

    Ratio 1.01

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ let validUsers = [
     "508122760644395009",
     "360925236243202059",
     "736824696271667300",
+    "446502936076615691",
 ];
 
 /*
@@ -29,6 +30,7 @@ let validUsers = [
 2: the hater
 3: the complainer
 4: Hdeth
+5: Darth Fetus
 */
 
 // mess end
@@ -44,12 +46,17 @@ const client = new Client({
     ],
 });
 
+// When bot first comes online.
 client.on("ready", (c) => {
     console.log(`Hello, ${c.user.tag}`);
+    // client.channels.cache.get("1218712862143414392").send({files: [{attachment: "src/images/drratio.png"}]});
 });
 
+// Scans each message sent.
 client.on("messageCreate", (msg) => {
-    console.log(`[${msg.guild.nameAcronym}] ${msg.author.globalName}: "${msg.content}"`); // logs discord user and their message
+    console.log(
+        `[${msg.guild.nameAcronym}] ${msg.author.globalName}: "${msg.content}"`
+    ); // logs discord user and their message
 
     if (msg.author.bot) {
         return;
@@ -63,20 +70,43 @@ client.on("messageCreate", (msg) => {
         return;
     }
 
+    // Performs commands if Dr. Ratio is pinged.
     function ifPinged() {
         if (canPing === true) {
             canPing = false;
             timer = setTimeout(toggleCanPing, 15000);
 
+            // CHECK FOR CONTENT SPECIFIC CASES
+            if (msg.content.toLowerCase().includes("fuck you")) {
+                msg.reply("fuck you too");
+                return;
+            } else if (msg.content.toLowerCase().includes("fuck off")) {
+                msg.channel.send("nah.");
+                return;
+            } else if (msg.content.toLowerCase().includes("kys")) {
+                msg.reply(`i'm going to cum on your forehead.`);
+                return;
+            } else if (msg.content.toLowerCase().includes("do you play")) {
+                msg.reply(`I will not belittle myself to play these so called "games".`);
+                return;
+            }
             // CHECKS FOR SPECIFIC USER CASES
-            if (validUsers.includes(msg.author.id)) {
+            else if (validUsers.includes(msg.author.id)) {
                 if (msg.author.id === validUsers[0]) {
                     // Case: Me
-                    msg.reply(`Hello, Founder ${msg.author.globalName}.`);
+                    msg.reply({
+                        // contents: `Hello, Founder ${msg.author.globalName}.`,
+                        content: "Wait... I've seen you before. Your ass ran it down 26 deaths in my game a few days ago." +
+                        "\n" +
+                        "https://iaace-na.github.io/lol-match-stats-viewer/?match=https%3A%2F%2Frewind.lol%2Fgetmatch%2FNA%2F4911615208%2Fstats",
+                        files: [{ attachment: "src/images/nunu1.png" }]
+                    });
                     return;
                 } else if (msg.author.id === validUsers[1]) {
                     // Case: Ushi
-                    msg.reply(`... Gragas?`);
+                    msg.reply({ 
+                        content: `... Gragas?` 
+                    });
                     return;
                 } else if (msg.author.id === validUsers[2]) {
                     // Case: the hater
@@ -84,7 +114,7 @@ client.on("messageCreate", (msg) => {
                         content:
                             "You are like a primitive version of me, except you are just racist." +
                             "\n" +
-                            "Also" +
+                            "Also:" +
                             "\n" +
                             "https://www.op.gg/summoners/na/OmlitD-NA1/matches/RvuDQoxvmIJG923-34PgH4t2b7h0jKssqEoBIWzr6HE%3D/1709951892000" +
                             "\n" +
@@ -101,28 +131,34 @@ client.on("messageCreate", (msg) => {
                     return;
                 } else if (msg.author.id === validUsers[4]) {
                     // Case: Hdeth
-                    msg.reply(
-                        `Fancy seeing you here today, ${msg.author.globalName}.`
-                    );
+                    msg.reply({
+                        contents: `Mind if we grab a cup of tea later this evening, ${msg.author.globalName}?`,
+                    });
+                    return;
+                } else if (msg.author.id === validUsers[5]) {
+                    // Case: Darth Fetus
+                    msg.reply({
+                        contents: "Wait... I've seen you before. Your ass ran it down 26 deaths in my game a few days ago." +
+                        "\n" +
+                        "https://iaace-na.github.io/lol-match-stats-viewer/?match=https%3A%2F%2Frewind.lol%2Fgetmatch%2FNA%2F4911615208%2Fstats",
+                        files: [{ attachment: "src/images/nunu1" }]
+                    });
                     return;
                 } else {
-                    msg.reply(`Hello, ${msg.author.globalName}.`);
+                    msg.reply({ 
+                        contents: `Hello, ${msg.author.globalName}.` 
+                    });
                     return;
                 }
+
+                // DEFAULTS
             } else {
-                if (msg.content.toLowerCase().includes("fuck you")) {
-                    msg.reply("fuck you too");
-                    return;
-                } else if (msg.content.toLowerCase().includes("fuck off")) {
-                    msg.channel.send("nah.");
-                    return;
-                } else {
-                    msg.reply("You dare ping me, foul monkey?");
-                }
+                msg.reply(`Hello, ${msg.author.globalName}.`);
             }
         }
     }
 
+    // Calls when "ratio" is mentioned.
     function ifReferenced() {
         if (canReference === true) {
             canReference = false;
