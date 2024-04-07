be665c5eb4d8a51ad273ac0eff287cd5a2b6d621
TerriblyFriedRice <148308017+TerriblyFriedRice@users.noreply.github.com>
2024-04-07 05:02:01 -0700

    npm install mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
+const { Client, IntentsBitField } = require("discord.js");
 const botCommands = require("./bot-commands.js");
 const eventHandler = require("./handlers/eventHandler.js");
 const userInteractions = require("./user-interactions.js");
-const { Client, IntentsBitField, ActivityType } = require("discord.js");
+const mongoose = require('mongoose');
+
 
 const client = new Client({
     intents: [
@@ -15,7 +17,14 @@ const client = new Client({
 });
 
 (async() => {
-    eventHandler(client);
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to DB.");
+    
+        eventHandler(client);
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
 })();
 
 client.login(process.env.TOKEN);
